83398b3d36d2a3b43cc18e47c13854a27fdf48a3
Marcos Bernal <cssgambaman@gmail.com>
2025-12-09 10:07:35 +0100

    Delete streams_coworking_center directory

--- streams_coworking_center/src/main/java/org/iesvdm/coworking/repositorio/ReservaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.iesvdm.coworking.repositorio;
-
-import org.iesvdm.coworking.modelo.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
